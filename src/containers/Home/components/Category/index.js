958d74cc63e1049921e97ba6d20b468d30a7acdf
YIZE <136076063@qq.com>
2019-06-19 13:57:13 +0800

    引入react-slick

---
+++ src/containers/Home/components/Category/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Category extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Category;
